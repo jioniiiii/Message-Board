923fe208526bde680f614b661e6315e0b91a503a
jioniiiii <lpolpo13.group@gmail.com>
2024-09-05 21:24:44 +0300

    Complete project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Message-Board
-A simple and responsive message board built with Express and EJS, featuring separate routes for viewing and adding messages.
+# Message Board
+    For future:
+    	-remake with react
---
+++ express-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes/index');
+
+const app = express();
+
+//set vies engine and folder
+app.set('view engine', 'ejs');
+app.set('vies', path.join(__dirname, 'views'));
+
+//middleware for static files and for form data
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+})
